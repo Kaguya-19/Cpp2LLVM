789172ab8918903976a23f2d6b3e7d2f04eb3b3a
JoelLei121 <76809658+JoelLei121@users.noreply.github.com>
2022-12-09 23:59:41 +0800

    add token id

--- myParser.py
+++ myParser.py
@@ -1,6 +1,15 @@
 from lex import Token
 
 class Parser():
+    tokenID = ['Alignas', 'Alignof', 'Asm', 'Auto', 'Bool', 'Break', 'Case', 'Catch', 'Char', 'Char16_t', 'Char32_t', 'Class', 'Const', 'Constexpr', 'Const_cast', 'Continue', 'Decltype', 'Default', 'Delete', 'Do', 
+                'Double', 'Dynamic_cast', 'Else', 'Enum', 'Explicit', 'Export', 'Extern', 'Float', 'For', 'Friend', 'Goto', 'If', 'Inline', 'Int', 'Long', 'Mutable', 'Namespace', 'New', 'Noexcept', 'Nullptr', 'Operator',
+                'Private', 'Register', 'Reinterpret_cast', 'Return', 'Short', 'Signed', 'Sizeof', 'Static', 'Static_assert', 'Static_cast', 'Struct', 'Switch', 'Template', 'This', 'Thread_local', 'Throw', 'Try', 'Typedef', 'Typeid', 'Typename', 
+                'Union', 'Unsigned', 'Using', 'Virtual', 'Void', 'Volatile', 'Wchar_t', 'While', 'Stack', 'push', 'empty', 'pop', 'top', 'Istream', 'Ostream', 'Istream_cin', 'Ostream_cout', 'Ostream_endl',
+                'String', 'Length', 'Vector', 'Find', 'Push_Back']
+    LiteralTokenID = ['ID', 'FloatingLiteral', 'HexadecimalLiteral', 'DecimalLiteral', 'OctalLiteral', 'BinaryLiteral', 'CharacterLiteral', 'BooleanLiteral', 'StringLiteral', 'MultiLineMacro', 'Directive']
+    optTokenID = ['LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET', 'SEMI', 'COMMA', 'DOT', 'PLUS', 'MINUS', 'MUL', 'DIV', 'MOD', 'INC', 'DEC', 'LSHIFT', 'RSHIFT', 'LT', 'LTE', 'GT', 'GTE', 'EQ', 'NEQ', 'AND', 'XOR', 'OR', 'LAND', 'LOR', 'NOT', 'BNOT',
+                'TERNARY', 'COLON', 'ASSIGN', 'ADD_ASSIGN', 'SUB_ASSIGN', 'MUL_ASSIGN', 'DIV_ASSIGN', 'MOD_ASSIGN', 'LSHIFT_ASSIGN', 'RSHIFT_ASSIGN', 'AND_ASSIGN', 'XOR_ASSIGN', 'OR_ASSIGN', 'ARROW', 'DOT_STAR', 'ELLIPSIS' , 'SCOPE']
+
     def __init__(self, tokens: list[Token]):
         self.tokens = tokens
         self.parseTree = {}
@@ -28,3 +37,5 @@ class Parser():
     def parse(self):
         
         pass
+
+    
