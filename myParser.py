f154146fa20f9e04b122a660be76d650da847c66
JoelLei121 <76809658+JoelLei121@users.noreply.github.com>
2022-12-09 18:05:39 +0800

    first try

---
+++ myParser.py
@@ -0,0 +1,11 @@
+from lex import Token
+
+class Parser():
+    def __init__(self, tokens: list):
+        self.tokens = tokens
+        pass
+
+    def parse(self):
+        jsonDict = {}
+        
+        pass
---
+++ test/test.cpp
@@ -0,0 +1,28 @@
+/*--------以下为了vS2019编译通过-------*/
+// #include <string>
+// #include <iostream>
+// #include<cstdio>
+// #include<vector>
+// #pragma warning(disable:4996)
+// using namespace std;
+
+/*------以下是使用自己编译器时的做法-------*/
+
+int main(){
+    printf("hello, world!");
+    return 0;
+}
+
+/*
+    json:
+    {
+        Function: {
+            Function: printf,
+            LPAREN: {
+                
+            }
+        }
+    }
+
+
+*/
